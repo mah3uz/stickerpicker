5d08d5d24fc042b27515bedb1c1d3a69faca9558
Mahfuz Shaikh <mah3uz@gmail.com>
2025-09-06 06:04:19 +0600

    Updated for darkroum

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '82bafefd'
+version = '0.1.0+dev.82bafefd'
+linkified_version = '0.1.0+dev.[82bafefd](https://github.com/maunium/stickerpicker/commit/82bafefd7d7918541a5ef586be092eb698f639fc)'
